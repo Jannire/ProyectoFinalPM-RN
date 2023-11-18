cd1732a67abfac6ccf9bdc63d29c45d5ecd901b2
Jannire Trujillo <Janniretruji@gmail.com>
2023-11-17 21:08:11 -0500

    Create app PM

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Hello, World!");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola trabajo de Programación Móvil estamos desde React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
