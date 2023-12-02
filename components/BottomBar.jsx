34adbac6f5e2c0f30bc0ff7242c92f67b4590fb0
Jannire Trujillo <Janniretruji@gmail.com>
2023-12-02 17:09:28 -0500

    Compartir finalizado

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { AlertDialog, Icon, ShareIcon, Box, Heading, AspectRatio, Image, Text, Center, HStack, Stack, NativeBaseProvider, View, Button } from "native-base";
+import { AlertDialog, Icon, ShareIcon, Heading, AspectRatio, Image, Text, Center, HStack, Stack, NativeBaseProvider, View, Button } from "native-base";
 import { StyleSheet, Linking } from 'react-native'
 import { Flex, Spacer, Input, VStack } from "native-base";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -14,60 +14,82 @@ const BottomBar = ({ navigation }) => {
     const onClose = () => setIsOpen(false);
     const cancelRef = React.useRef(null);
 
-    const initiateWhatsApp = () => {
+    const AbrirWhatsapp = () => { // CAMBIAR PARA ESCOGER A QUIEN MANDAR
         let url =
-          'whatsapp://send?text=' + 
-           "Hola! Puedes revisar nuestro proyecto en el siguiente link: https://github.com/Jannire/PM-Entrega01Grupal" +
-          '&phone=51' + "955198135";
+            'whatsapp://send?text=' +
+            "Hola! Puedes revisar nuestro proyecto en el siguiente link: https://github.com/Jannire/ProyectoFinalPM-RN" +
+            '&phone=51' + "955198135";
         Linking.openURL(url)
-          .then((data) => {
-            console.log('WhatsApp Opened');
-          })
-          .catch(() => {
-            alert('Make sure Whatsapp installed on your device');
-          });
+            .then((data) => {
+                console.log('Mandar mensaje');
+            })
+            .catch(() => {
+                alert('Hay un error: Whatsapp Compartir');
+            });
+    };
+
+    const AbrirGithub = () => {
+        let url = 'https://github.com/Jannire/ProyectoFinalPM-RN';
+        Linking.openURL(url)
+            .then((data) => {
+                console.log('Abrir repo en Github');
+            })
+            .catch(() => {
+                alert('Hay un error: Github Compartir');
+            });
     };
 
     return (
         <View>
             <HStack bg="orange.400" h="30%">
                 <Center h="100%" w="33%">
-                    <Button  variant="ghost" w="100%" h="100%" onPress={() => navigation.navigate("Home")}>
+                    <Button variant="ghost" w="100%" h="100%" onPress={() => navigation.navigate("Home")}>
                         <Icon style={styles.tabs} marginLeft="2.5" viewBox="0 0 24 24"><Path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></Icon>
-                        <Text style={styles.tabs} marginBottom="3">Home</Text> 
+                        <Text style={styles.tabs} marginBottom="3">Home</Text>
                     </Button>
                 </Center>
                 <Center w="33%">
                     <Button variant="ghost" w="100%" h="100%" onPress={() => navigation.navigate("Ejercicios")}>
                         <Icon style={styles.tabs} marginLeft="5" viewBox="0 0 24 24"><Path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></Icon>
-                        <Text style={styles.tabs} marginBottom="3">Ejercicios</Text> 
+                        <Text style={styles.tabs} marginBottom="3">Ejercicios</Text>
                     </Button>
                 </Center>
                 <Center w="33%">
                     <Button variant="ghost" w="100%" h="100%" onPress={() => setIsOpen(!isOpen)}>
-                        <ShareIcon style={styles.tabs} marginLeft="5"/>
-                        <Text style={styles.tabs} marginBottom="3">Compartir</Text> 
+                        <ShareIcon style={styles.tabs} marginLeft="5" />
+                        <Text style={styles.tabs} marginBottom="3">Compartir</Text>
                     </Button>
                 </Center>
             </HStack>
 
             <AlertDialog leastDestructiveRef={cancelRef} isOpen={isOpen} onClose={onClose}>
                 <AlertDialog.Content>
-                    
+
                     <AlertDialog.Header>Compartir App</AlertDialog.Header>
                     <AlertDialog.Body>
-                        <Button>
-                            aaa
-                        </Button>
-                        Compartir
+                        <Center>
+                        <HStack space={2}>
+                            <Center>
+                                <Button variant="unstyled" onPress={AbrirWhatsapp}>
+                                    <Image source={{
+                                        uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Whatsapp_logo_svg.png/600px-Whatsapp_logo_svg.png"
+                                    }} alt="Logo Whatsapp" size="sm" />
+                                </Button>
+                            </Center>
+                            <Center>
+                                <Button variant="unstyled" onPress={AbrirGithub}>
+                                    <Image source={{
+                                        uri: "https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                                    }} alt="Logo Github" size="sm" />
+                                </Button>
+                            </Center>
+                        </HStack>
+                        </Center>
                     </AlertDialog.Body>
                     <AlertDialog.Footer>
                         <Button.Group space={2}>
                             <Button colorScheme="warning" onPress={onClose}>
                                 Aceptar
-                            </Button>
-                            <Button colorScheme="warning" onPress={initiateWhatsApp}>
-                                WH
                             </Button>
                         </Button.Group>
                     </AlertDialog.Footer>
